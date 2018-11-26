a00bb20ec6683a4b7a30b1a51dfde5b85cfaafc6
wengzhongjie <1310120194@qq.com>
2018-11-26 15:13:13 +0800

    提交到dev分支

--- src/main/java/com/tb121/test/Student.java
+++ src/main/java/com/tb121/test/Student.java
@@ -4,4 +4,8 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("这是主分支");
     }
+
+    private void song() {
+        System.out.println("这是dev分支");
+    }
 }
